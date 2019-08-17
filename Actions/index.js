41770049e5e55738d8ba4d46081b7c39d42b659d
yolanlepibrac <46227887+yolanlepibrac@users.noreply.github.com>
2019-08-17 02:20:15 +0200

    focus on search

---
+++ Actions/index.js
@@ -0,0 +1,41 @@
+
+import { ADD_CATEGORY } from "../Constants/action-types";
+import { ADD_KEYWORD } from "../Constants/action-types";
+import { RESET_CATEGORY } from "../Constants/action-types";
+import { RESET_KEYWORD } from "../Constants/action-types";
+import { SET_USEREMAIL } from "../Constants/action-types";
+import { SET_ACCOUNTSTATE } from "../Constants/action-types";
+import { DISPLAY_LOADING } from "../Constants/action-types";
+import { CONNECT } from "../Constants/action-types";
+
+export function addCategory(category) {
+  return { type: ADD_CATEGORY, category }
+};
+
+export function addKeyword(keyWord) {
+  return { type: ADD_KEYWORD, keyWord }
+};
+
+export function resetCategory() {
+  return { type: RESET_CATEGORY }
+};
+
+export function resetKeyword() {
+  return { type: RESET_KEYWORD }
+};
+
+export function setUserEmail(email) {
+  return { type: SET_USEREMAIL, email }
+};
+
+export function changeAccountState(accountState) {
+  return { type: SET_ACCOUNTSTATE, accountState }
+};
+
+export function displayLoading(displayLoading) {
+  return { type: DISPLAY_LOADING, displayLoading }
+};
+
+export function connectAccount(connected) {
+  return { type: CONNECT, connected }
+};
---
+++ Constants/action-types.js
@@ -0,0 +1,10 @@
+
+
+export const ADD_CATEGORY = "ADD_CATEGORY";
+export const ADD_KEYWORD = "ADD_KEYWORD";
+export const RESET_CATEGORY = "RESET_CATEGORY";
+export const RESET_KEYWORD = "RESET_KEYWORD";
+export const SET_USEREMAIL = "SET_USEREMAIL";
+export const SET_ACCOUNTSTATE = "SET_ACCOUNTSTATE";
+export const DISPLAY_LOADING = "DISPLAY_LOADING";
+export const CONNECT = "CONNECT";
